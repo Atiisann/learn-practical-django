1b43372029404828a53f885ea32958e345394f27
Tomoki <tomokkin@icloud.com>
2021-08-29 14:36:43 +0900

    initialize Accounts

--- djangosnippets/djangosnippets/urls.py
+++ djangosnippets/djangosnippets/urls.py
@@ -7,6 +7,7 @@ from snippets.views import top
 
 urlpatterns = [
     path('', top, name='top'),
-    path('snippets/', include('snippets.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('snippets/', include('snippets.urls')),
 ]
